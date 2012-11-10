2691fc5b3b50b65eb785911cb9a487238d288b4d
Adam Phelps <amp@cs.stanford.edu>
2012-11-09 17:24:31 -0800

    * Initial putback of custom sign code

--- UmbrellaSign/UmbrellaSign.ino
+++ UmbrellaSign/UmbrellaSign.ino
@@ -1,55 +1,89 @@
 /*
-    A circular light buffer.  If you manage to construct a circle of LEDs,
-    definitely send in pictures.  What this sketch does is take an analog
-    reading off of analog pin 0 and add it to the current value of the last LED.
-    If the resultant sum is greater than 4095, it turns the LED off,
-    otherwise sets LED 0 to the value of the sum.
-
-    If you ground pin 12, it will set LED 0 to zero.
-
-    Then it shifts all the LED values up one (so LED 0 becomes LED 1) and sets
-    LED 0 to the value shifted off the last LED (so if one LED is on, it will
-    go in a circle forever).
-
-    See the BasicUse example for hardware setup.
-
-    Alex Leone <acleone ~AT~ gmail.com>, 2009-02-04 */
+ * This code drive's Adam's Umbrella sign.
+ *
+ * Internal to the sign is an Arduino Nano v3 driving two serially connected
+ * TLC5940 LED drivers for the individual pixels of the sign.
+ */
 
 #include "Arduino.h"
 
 #include "Tlc5940.h"
 #include "tlc_shifts.h"
 
-// which analog pin to use
-#define ANALOG_PIN      0
-
-// which pin to clear the LEDs with
-#define CLEAR_PIN      12
-
 #if NUM_TLCS != 2
+  /* NUM_TLCS must be set to 2 in tlc_config.h */
   NUM_TLCS must equal 2;
 #endif
 
-// how many millis for one full revolution over all the LEDs
-#define SCOPE_PERIOD    (2000 * NUM_TLCS)
-#define LED_PERIOD      SCOPE_PERIOD / (NUM_TLCS * 16)
+/* Array to map the light positions in the sign to the LED driver pins */
+uint8_t signTLCPin[] =
+{
+  XX, XX, XX, XX, XX, -1, -1, -1,
+  -1, -1, XX, XX, XX, XX, XX, -1,
+  -1, -1, -1, XX, XX, XX, XX, -1,
+  -1, -1, -1, -1, XX, XX, XX, XX,
+  -1, -1, -1, -1, -1, XX, XX, XX,
+  -1, -1, XX, -1, -1, -1, XX, XX
+  -1, XX, -1, -1, -1, -1, -1, XX,
+  XX, -1, -1, -1, -1, -1, -1, XX,
+};
+
+/* Mapping of LEDs into rows along the diaginal axis */
+uint8_t signRows[] =
+{
+   5,  4,  3,  2,  1, -1, -1, -1,
+  -1, -1,  4,  3,  2,  1,  0, -1,
+  -1, -1, -1,  4,  3,  2,  1, -1,
+  -1, -1, -1, -1,  4,  3,  2,  1,
+  -1, -1, -1, -1, -1,  4,  3,  2,
+  -1, -1,  6, -1, -1, -1,  4,  3
+  -1,  7, -1, -1, -1, -1, -1,  4,
+   8, -1, -1, -1, -1, -1, -1,  5,
+};
+
+/* TCL Pin values */
+#define NUM_LEDS 28
+uint16_t ledValues[NUM_LEDS] = {
+  4095, 4095, 4095, 4095, 4095, 4095, 4095, 4095,
+  4095, 4095, 4095, 4095, 4095, 4095, 4095, 4095,
+  4095, 4095, 4095, 4095, 4095, 4095, 4095, 4095,
+  4095, 4095, 4095, 4095, 4095, 4095, 4095, 4095,
+};
+
+
+/* Period to delay between shifts, in milliseconds */
+#define LED_PERIOD 10
 
 void setup()
 {
-  pinMode(CLEAR_PIN, INPUT);
-  digitalWrite(CLEAR_PIN, HIGH); // enable pull-up
-  Tlc.init();
+  /* Initialize the LED drivers with all-on */ 
+  Tlc.init(4095);
+
+  /* Initialize the LED values */
+  for (int led = 0; led < NUM_LEDS; led++) {
+    ledValues = 4095;
+  }
 }
 
+int mode = 0;
 void loop()
 {
-  // shiftUp returns the value shifted off the last pin
-  uint16_t sum = tlc_shiftUp() + 512 * 4;
-//  if (digitalRead(CLEAR_PIN) == LOW || sum > 4095)
-  if (sum > 4095)
-    sum = 0;
-  Tlc.set(0, sum);
-  Tlc.update();
+  /* Modify the LED values based on the current mode */
+  switch (mode) {
+      case 0: 
+      {
+        
+        break;
+      }
+    
+  }
+
+  /* Update all TLC
+  for (int led = 0; led < NUM_LEDS; led++) {
+    Tlc.set(led, ledValues[led]);
+  }
+  while (Tlc.update()) // Wait until the data has been sent to the TLCs;
+
   delay(LED_PERIOD);
 }
 
