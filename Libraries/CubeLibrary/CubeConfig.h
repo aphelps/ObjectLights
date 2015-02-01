ee4d376df9748b719d399a8d727fd40045c3eb32
Adam Phelps <amp@opendns.com>
2015-01-31 19:50:32 -0800

    Fixed the CubeLights.h for non-BigCube units

--- Libraries/CubeLibrary/CubeConfig.h
+++ Libraries/CubeLibrary/CubeConfig.h
@@ -17,41 +17,20 @@
 
 #define BIG_CUBE    5
 
-#define CUBE_NUMBER BIG_CUBE
+#define CUBE_NUMBER ADAM_CUBE
 
 #define CUBE_VERSION 1
 
 #define CAP_TOUCH_IRQ 2
 
 // Touch sensor trigger and release values
-#if CUBE_NUMBER == PARENT_CUBE
-  #define CAP_SENSOR_1 0
-  #define CAP_SENSOR_2 1
-  #define CAP_SENSOR_1_TOUCH 6
-  #define CAP_SENSOR_1_RELEASE 2
-  #define CAP_SENSOR_2_TOUCH 6
-  #define CAP_SENSOR_2_RELEASE 2
-#elif CUBE_NUMBER == ADAM_CUBE
-  #define CAP_SENSOR_1 0
-  #define CAP_SENSOR_2 1
-  #define CAP_SENSOR_1_TOUCH 15
-  #define CAP_SENSOR_1_RELEASE 2
-  #define CAP_SENSOR_2_TOUCH 15
-  #define CAP_SENSOR_2_RELEASE 2
-#elif CUBE_NUMBER == BIG_CUBE
+#if CUBE_NUMBER == BIG_CUBE
+#warning CUBE_NUMBER IS SET TO BIG_CUBE!!!
   #define CAP_SENSOR_1 11
   #define CAP_SENSOR_2 9
-  #define CAP_SENSOR_1_TOUCH 15
-  #define CAP_SENSOR_1_RELEASE 2
-  #define CAP_SENSOR_2_TOUCH 15
-  #define CAP_SENSOR_2_RELEASE 2
 #else
   #define CAP_SENSOR_1 0
   #define CAP_SENSOR_2 1
-  #define CAP_SENSOR_1_TOUCH 10
-  #define CAP_SENSOR_1_RELEASE 2
-  #define CAP_SENSOR_2_TOUCH 10
-  #define CAP_SENSOR_2_RELEASE 2
 #endif
 
 // Some pixel strands may need to start after the initial LED
